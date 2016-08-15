388417de10a0a76593eff0ea0e8c0fefdafeee64
Wbq896990 <1006319834@qq.com>
2016-08-15 17:16:39 +0800

    新增数据库帮助类，各实体类。

--- app/src/main/java/org/josh/weather/activity/MainActivity.java
+++ app/src/main/java/org/josh/weather/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package org.josh.weather;
+package org.josh.weather.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.josh.weather.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/org/josh/weather/model/Province.java
@@ -0,0 +1,35 @@
+package org.josh.weather.model;
+
+/**
+ * Created by Josh on 2016/8/15.
+ */
+public class Province {
+
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
